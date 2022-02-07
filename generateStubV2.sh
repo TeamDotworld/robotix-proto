9d1f5dd31a4f14b3a4c55db87cc143b339ee8319
anand-dotworld <anand.s@dotworld.in>
2022-02-07 19:09:53 +0530

    updated typo errors

--- generateStubV2.sh
+++ generateStubV2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# protoc --go_out=plugins=grpc:/home/anand/Work/golang/src/robotix-agent rcc.proto
-# protoc --go_out=plugins=grpc:/home/anand/Work/golang/src/robotix-command-center rcc.proto
-python -m grpc_tools.protoc -I. --python_out=/home/anand/Work/golang/src/robotix-agent/ros_node/protos --grpc_python_out=/home/anand/Work/golang/src/robotix-agent/ros_node/protos rcc.proto
+protoc --go_out=plugins=grpc:/home/anand/go/src/github.com/anand-dotworld/robotix-agent rcc.proto
+protoc --go_out=plugins=grpc:/home/anand/go/src/github.com/anand-dotworld/robotix-command-center rcc.proto
+# python -m grpc_tools.protoc -I. --python_out=/home/anand/Work/golang/src/robotix-agent/ros_node/protos --grpc_python_out=/home/anand/Work/golang/src/robotix-agent/ros_node/protos rcc.proto
 # # cp rcc.proto /home/anand/Work/golang/src/delivery-robot-backend/protos/rcc.proto
 
 # PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
