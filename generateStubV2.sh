7140de178bb33b6375110cad6b3488cf92732586
anand-dotworld <anand.s@dotworld.in>
2022-07-19 10:57:54 +0530

    Added new services and refactored old services

--- generateStubV2.sh
+++ generateStubV2.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-protoc --go_out=plugins=grpc:/home/anand/go/src/github.com/anand-dotworld/robotix-agent rcc.proto
-protoc --go_out=plugins=grpc:/home/anand/go/src/github.com/anand-dotworld/robotix-command-center rcc.proto
-# python -m grpc_tools.protoc -I. --python_out=/home/anand/Work/golang/src/robotix-agent/ros_node/protos --grpc_python_out=/home/anand/Work/golang/src/robotix-agent/ros_node/protos rcc.proto
-# # cp rcc.proto /home/anand/Work/golang/src/delivery-robot-backend/protos/rcc.proto
+protoc --go_out=plugins=grpc:/home/anand/go/src/ ./protos/rcc/v1/rcc.proto
+# protoc --go_out=plugins=grpc:/home/anand/go/src/github.com/anand-dotworld/robotix-proto ./go-multi-tenant/agent/v1/agent.proto
+
+
+# python3 -m grpc_tools.protoc -I. --python_out=/home/anand/go/src/github.com/anand-dotworld/robotix-agent/node --grpc_python_out=/home/anand/go/src/github.com/anand-dotworld/robotix-agent/node ./go-multi-tenant/agent/v1/agent.proto
+
+# # cp rcc.proto /home/anand/Work/golang/src/delivery-robot-backend/go-multi-tenant/rcc.proto
+
+for f in ./protos/model/v1/*.proto 
+do 
+    protoc --go_out=plugins=grpc:/home/anand/go/src/ $f
+    echo "Processing $f file.."
+done
+
 
 # PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 # GRPC_TOOLS_NODE_PROTOC_PLUGIN="./node_modules/.bin/grpc_tools_node_protoc_plugin"
 # GRPC_TOOLS_NODE_PROTOC="./node_modules/.bin/grpc_tools_node_protoc"
 # f="./"
-# # f="./protos"
+# # f="./go-multi-tenant"
 # # for f in ./rcc.proto; do
 
 # #   # skip the non proto files
@@ -32,4 +42,39 @@ protoc --go_out=plugins=grpc:/home/anand/go/src/github.com/anand-dotworld/roboti
 #       -I . \
 #       "./rcc.proto"
 
+# done
+
+
+
+
+
+
+
+
+declare -a packages=("rcc-multi-tenant" "robotix-agent" "element3")
+for p in "${packages[@]}"
+do
+   echo "$p"
+   # or do whatever with individual element of the array
+done
+# for f in ./model/v1/*.proto 
+# do 
+#     sed -i 's+go_package="";+go_package="rcc-multi-tenant/protos/v1/model";+g' $f
+#     echo "writing $f file.."
+# done
+
+# for f in ./model/v1/*.proto 
+# do 
+#     protoc --go_out=plugins=grpc:/home/anand/go/src/github.com/anand-dotworld/ $f
+#     echo "Processing $f file.."
+# done
+# sed -i 's+go_package="";+go_package="rcc-multi-tenant/protos/v1/agent";+g' ./protos/rcc/v1/rcc.proto
+# protoc --go_out=plugins=grpc:/home/anand/go/src/github.com/anand-dotworld/ ./protos/rcc/v1/rcc.proto
+# sed -i 's+go_package="rcc-multi-tenant/protos/v1/agent";+go_package="";+g' ./protos/rcc/v1/rcc.proto
+
+
+# for f in ./model/v1/*.proto 
+# do 
+#     sed -i 's+go_package="rcc-multi-tenant/protos/v1/model";+go_package="";+g' $f
+#     echo "removing $f file.."
 # done
