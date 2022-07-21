72231ef89b4dfe03fdaafc02c06ad74d290d7e7b
anand-dotworld <anand.s@dotworld.in>
2022-07-21 13:31:18 +0530

    updated health rpc service

--- generate.sh
+++ generate.sh
@@ -4,7 +4,7 @@ protoc --go_out=plugins=grpc:/home/anand/go/src/ ./protos/rcc/v1/rcc.proto
 protoc --go_out=plugins=grpc:/home/anand/go/src/ ./protos/agent/v1/agent.proto
 
 
-# python3 -m grpc_tools.protoc -I. --python_out=/home/anand/go/src/github.com/anand-dotworld/robotix-agent/node --grpc_python_out=/home/anand/go/src/github.com/anand-dotworld/robotix-agent/node ./go-multi-tenant/agent/v1/agent.proto
+python3 -m grpc_tools.protoc -I. --python_out=/home/anand/go/src/github.com/anand-dotworld/robotix-agent/node --grpc_python_out=/home/anand/go/src/github.com/anand-dotworld/robotix-agent/node ./protos/agent/v1/agent.proto
 
 # # cp rcc.proto /home/anand/Work/golang/src/delivery-robot-backend/go-multi-tenant/rcc.proto
 
@@ -42,39 +42,4 @@ done
 #       -I . \
 #       "./rcc.proto"
 
-# done
-
-
-
-
-
-
-
-
-declare -a packages=("rcc-multi-tenant" "robotix-agent" "element3")
-for p in "${packages[@]}"
-do
-   echo "$p"
-   # or do whatever with individual element of the array
-done
-# for f in ./model/v1/*.proto 
-# do 
-#     sed -i 's+go_package="";+go_package="rcc-multi-tenant/protos/v1/model";+g' $f
-#     echo "writing $f file.."
-# done
-
-# for f in ./model/v1/*.proto 
-# do 
-#     protoc --go_out=plugins=grpc:/home/anand/go/src/github.com/anand-dotworld/ $f
-#     echo "Processing $f file.."
-# done
-# sed -i 's+go_package="";+go_package="rcc-multi-tenant/protos/v1/agent";+g' ./protos/rcc/v1/rcc.proto
-# protoc --go_out=plugins=grpc:/home/anand/go/src/github.com/anand-dotworld/ ./protos/rcc/v1/rcc.proto
-# sed -i 's+go_package="rcc-multi-tenant/protos/v1/agent";+go_package="";+g' ./protos/rcc/v1/rcc.proto
-
-
-# for f in ./model/v1/*.proto 
-# do 
-#     sed -i 's+go_package="rcc-multi-tenant/protos/v1/model";+go_package="";+g' $f
-#     echo "removing $f file.."
 # done
