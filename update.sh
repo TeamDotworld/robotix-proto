96ed04318b43cef48542adffc5562c16c9483184
anand-dotworld <anand.s@dotworld.in>
2022-08-27 18:37:29 +0530

    added webrtc service

--- update.sh
+++ update.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-version=24
+version=25
 old_version=$version
 
-echo "Generating proto code"
+echo -e '\033[4;32;1mGenerating Proto Code\033[m'
 ./generate.sh
 
-echo "Adding to git..."
+echo -e '\033[4;32;1mAdding To Git\033[m'
 git add .
-echo "Give a message: "
+echo -e '\033[4;32;1mGive a message: \033[m'
 read message
 git commit -m "$message"
 version=$((version+1))
 echo "Creating a tag - v1.0.$version"
 git tag "v1.0.$version"
-echo "Pushing code to github"
+echo -e '\033[4;32;1mPushing Code To Github\033[m'
 git push origin v2
 git push --tags
-echo "Updating go repository"
+echo -e '\033[4;32;1mUpdating Go Repository\033[m'
 
 cd /home/anand/go/src/github.com/anand-dotworld/robotix-agent
 GOPRIVATE=github.com/TeamDotworld/robotix-proto go get github.com/TeamDotworld/robotix-proto@v1.0.$version
@@ -26,6 +26,5 @@ GOPRIVATE=github.com/TeamDotworld/robotix-proto go get github.com/TeamDotworld/r
 go mod tidy
 
 sed -i "s/version=$old_version/version=$version/g" /home/anand/go/src/github.com/TeamDotworld/robotix-proto/update.sh
-echo "Done!"
-# git tag 
+echo -e '\033[4;32;1mDone!\033[m'
 
