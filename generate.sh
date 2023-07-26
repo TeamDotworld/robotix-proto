9eaa3ccd2b2531757e8669e379e084544e45e994
amizhthan-dotworld <amizhthan.m@dotworld.in>
2023-07-26 17:42:30 +0530

    added id in webrtcsignalrequest message

--- generate.sh
+++ generate.sh
@@ -3,20 +3,20 @@
 # The protoc compiles the proto code to go code. The files will be generated in the following directory
 # /home/anand/go/src/github.com/TeamDotworld/robotix-proto/v1
 # The folder structure for this repository should be like above. change the username
-protoc --go_out=plugins=grpc:/home/anand/go/src/ ./protos/rcc/v1/rcc.proto
-protoc --go_out=plugins=grpc:/home/anand/go/src/ ./protos/node_sdk/v1/node_sdk.proto
-protoc --go_out=plugins=grpc:/home/anand/go/src/ ./protos/agent/v1/agent.proto
+protoc --go_out=plugins=grpc:/home/amizhthan/go/src/ ./protos/rcc/v1/rcc.proto
+protoc --go_out=plugins=grpc:/home/amizhthan/go/src/ ./protos/node_sdk/v1/node_sdk.proto
+protoc --go_out=plugins=grpc:/home/amizhthan/go/src/ ./protos/agent/v1/agent.proto
 
 # This line is for compiling python files for the agent.proto file
-python3 -m grpc_tools.protoc -I. --python_out=/home/anand/go/src/github.com/anand-dotworld/robotix-agent/node --grpc_python_out=/home/anand/go/src/github.com/anand-dotworld/robotix-agent/node ./protos/agent/v1/agent.proto
+#python3 -m grpc_tools.protoc -I. --python_out=/home/amizhthan/go/src/github.com/anand-dotworld/robotix-agent/node --grpc_python_out=/home/anand/go/src/github.com/anand-dotworld/robotix-agent/node ./protos/agent/v1/agent.proto
 
 # # cp rcc.proto /home/anand/Work/golang/src/delivery-robot-backend/go-multi-tenant/rcc.proto
 
 # This will loop over all the model files and create both go and python files.
 for f in ./protos/model/v1/*.proto 
 do 
-    protoc --go_out=plugins=grpc:/home/anand/go/src/ $f
-    python3 -m grpc_tools.protoc -I. --python_out=/home/anand/go/src/github.com/anand-dotworld/robotix-agent/node --grpc_python_out=/home/anand/go/src/github.com/anand-dotworld/robotix-agent/node $f
+    protoc --go_out=plugins=grpc:/home/amizhthan/go/src/ $f
+    #python3 -m grpc_tools.protoc -I. --python_out=/home/amizhthan/go/src/github.com/anand-dotworld/robotix-agent/node --grpc_python_out=/home/anand/go/src/github.com/anand-dotworld/robotix-agent/node $f
 
     echo "Processing $f file.."
 done
--- go.mod
+++ go.mod
@@ -3,14 +3,15 @@ module github.com/TeamDotworld/robotix-proto
 go 1.17
 
 require (
-	github.com/golang/protobuf v1.5.2
-	google.golang.org/grpc v1.48.0
-	google.golang.org/protobuf v1.28.0
+	github.com/golang/protobuf v1.5.3
+	google.golang.org/grpc v1.56.2
+	google.golang.org/protobuf v1.31.0
 )
 
 require (
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
-	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
-	golang.org/x/text v0.3.3 // indirect
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	google.golang.org/genproto v0.0.0-20230706204954-ccb25ca9f130 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230725213213-b022f6e96895 // indirect
 )
