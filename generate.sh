25dd6f324ca4b7979d4cbba5dade9e6bd6b35874
amizhthan-dotworld <amizhthan.m@dotworld.in>
2024-05-27 10:44:13 +0530

    added device model in device specification model

--- generate.sh
+++ generate.sh
@@ -3,9 +3,12 @@
 # The protoc compiles the proto code to go code. The files will be generated in the following directory
 # /home/anand/go/src/github.com/TeamDotworld/robotix-proto/v1
 # The folder structure for this repository should be like above. change the username
-protoc --go_out=plugins=grpc:/home/amizhthan/go/src/ ./protos/rcc/v1/rcc.proto
-protoc --go_out=plugins=grpc:/home/amizhthan/go/src/ ./protos/node_sdk/v1/node_sdk.proto
-protoc --go_out=plugins=grpc:/home/amizhthan/go/src/ ./protos/agent/v1/agent.proto
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    ./protos/rcc/v1/rcc.proto
+# ./protos/rcc/v1/rcc.proto
+# protoc --go_out=plugins=grpc:/home/amizhthan/go/src/ ./protos/node_sdk/v1/node_sdk.proto
+# protoc --go_out=plugins=grpc:/home/amizhthan/go/src/ ./protos/agent/v1/agent.proto
 
 # This line is for compiling python files for the agent.proto file
 python3 -m grpc_tools.protoc -I. --python_out=/home/amizhthan/go/src/github.com/robotix-agent/node --grpc_python_out=/home/amizhthan/go/src/github.com/robotix-agent/node ./protos/agent/v1/agent.proto
@@ -15,7 +18,10 @@ python3 -m grpc_tools.protoc -I. --python_out=/home/amizhthan/go/src/github.com/
 # This will loop over all the model files and create both go and python files.
 for f in ./protos/model/v1/*.proto 
 do 
-    protoc --go_out=plugins=grpc:/home/amizhthan/go/src/ $f
+    # protoc --go_out=plugins=grpc:/home/amizhthan/go/src/ $f
+    protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    $f
     python3 -m grpc_tools.protoc -I. --python_out=/home/amizhthan/go/src/github.com/robotix-agent/node --grpc_python_out=/home/amizhthan/go/src/github.com/robotix-agent/node $f
 
     echo "Processing $f file.."
--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/TeamDotworld/robotix-proto
 go 1.17
 
 require (
-	github.com/golang/protobuf v1.5.3
+	github.com/golang/protobuf v1.5.4
 	google.golang.org/grpc v1.56.2
-	google.golang.org/protobuf v1.31.0
+	google.golang.org/protobuf v1.33.0
 )
 
 require (
