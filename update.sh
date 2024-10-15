d5ad575a39f034e32252a620d2f4f7a78ddf42fc
cipher <cipher@kali.kali>
2024-10-15 14:24:44 +0530

    Updated fleet manager

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@
 # - Generate proto files
 # - Add to git, commit, update version, create tag, push both tag and commit.
 
-version=91
+version=92
 old_version=$version
 
 echo -e '\033[4;32;1mGenerating Proto Code\033[m'
