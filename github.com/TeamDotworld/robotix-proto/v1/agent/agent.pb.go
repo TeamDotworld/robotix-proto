a8d236f76941a5f9d01bf8fd5c1d21a4f8dc6b9f
cipher <cipher@kali.kali>
2024-10-15 15:20:30 +0530

    [fix]: incorrect package path

--- github.com/TeamDotworld/robotix-proto/v1/agent/agent.pb.go
+++ github.com/TeamDotworld/robotix-proto/v1/agent/agent.pb.go
@@ -7,7 +7,7 @@
 package agent
 
 import (
-	model "/github.com/TeamDotworld/robotix-proto/protos/v1/model"
+	model "github.com/TeamDotworld/robotix-proto/protos/v1/model"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
--- github.com/TeamDotworld/robotix-proto/v1/agent/agent_grpc.pb.go
+++ github.com/TeamDotworld/robotix-proto/v1/agent/agent_grpc.pb.go
@@ -7,8 +7,8 @@
 package agent
 
 import (
-	model "/github.com/TeamDotworld/robotix-proto/protos/v1/model"
 	context "context"
+	model "github.com/TeamDotworld/robotix-proto/protos/v1/model"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
